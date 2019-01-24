3bef830a541f6b924694a125f68c00155ff3f81b
hunterman2244 <hunterringland@gmail.com>
2019-01-24 10:51:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
+    client.user.setActivity('porn', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
+    if (!msg.content.startsWith(A ) || !msg.guild) return;
     if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
-    else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+    else if (command === 'invite') return msg.channel.send();
+    else if (command === 'AA') return msg.channel.send(A AA);
 });
 
 client.login('NTM4MDAwOTkwODE3NzQ2OTU3.DytklQ.DhA6CphMC8-_vY9mQxZeOhEIRmU');
