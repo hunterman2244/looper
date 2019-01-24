032dd806b2d06170c816e8406f15683f434e005f
hunterman2244 <hunterringland@gmail.com>
2019-01-24 10:58:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
+    if (!msg.content.startsWith(A) || !msg.guild) return;
+    const command = msg.content.split(' ')[0].substr(1);
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
     else if (command === 'invite') return msg.channel.send();
-    else if (command === 'AA') return msg.channel.send(A AA);
+    else if (command === 'AA') return msg.channel.send(AAA);
 });
 
 client.login('NTM4MDAwOTkwODE3NzQ2OTU3.DytklQ.DhA6CphMC8-_vY9mQxZeOhEIRmU');
