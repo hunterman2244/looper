656e2f71ffb427a87bfb466bf522dc7091c23a98
hunterman2244 <hunterringland@gmail.com>
2019-01-24 10:51:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('porn', {type: 'WATCHING'});
+    client.user.setActivity('youtube', {type: 'WATCHING'});
 });
 
 client.on('message', msg => {
